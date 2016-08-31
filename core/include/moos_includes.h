d52019b0432c422cfb2e66293e8d2622ba03c232
zhouxiang <zhouxiangbetter@163.com>
2016-08-31 15:36:30 +0800

    @^@

---
+++ core/include/moos_includes.h
@@ -0,0 +1,14 @@
+#ifndef MOOS_INCLUDES_H
+#define MOOS_INCLUDES_H
+
+
+#include "moos_looper.h"
+#include "moos_logger.h"
+#include "moos_object.h"
+#include "moos_signal.h"
+#include "moos_thread.h"
+
+
+
+
+#endif // MOOS_INCLUDES_H
