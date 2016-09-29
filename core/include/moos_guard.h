a8d32b68b506d677858ef4590177b04f633afec6
Xiang Zhou <ZhouBox@users.noreply.github.com>
2016-09-29 13:27:46 +0800

    Create moos_guard.h
    add guard

---
+++ core/include/moos_guard.h
@@ -0,0 +1,27 @@
+#ifndef MOOS_GUARD_H
+#define MOOS_GUARD_H
+
+
+#include "moos_defines.h"
+
+#include <atomic>
+
+DEFINE_NAMESPACE_MOOS_BEGIN
+
+
+class MoosGuard
+{
+  public:
+  
+  private:
+  
+  
+};
+
+
+
+
+
+DEFINE_NAMESPACE_MOOS_END
+
+#endif // MOOS_GUARD_H
